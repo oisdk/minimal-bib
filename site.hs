0608668f8d9f6154fee10a1ab555ecf00a884014
oisdk <oisin.kidney@gmail.com>
2016-10-01 01:06:04 +0100

    Initial dump

---
+++ site.hs
@@ -0,0 +1,88 @@
+--------------------------------------------------------------------------------
+{-# LANGUAGE OverloadedStrings #-}
+import           Control.Monad
+import           Data.Monoid   (mappend)
+import           Hakyll
+import           Text.Pandoc   (Pandoc)
+
+--------------------------------------------------------------------------------
+main :: IO ()
+main = hakyll $ do
+    match "images/*" $ do
+        route   idRoute
+        compile copyFileCompiler
+
+    match "css/*" $ do
+        route   idRoute
+        compile compressCssCompiler
+
+    match (fromList ["about.rst", "contact.markdown"]) $ do
+        route   $ setExtension "html"
+        compile $ pandocCompiler
+            >>= loadAndApplyTemplate "templates/default.html" defaultContext
+            >>= relativizeUrls
+
+    match "posts/*" $ do
+        route $ setExtension "html"
+        compile $ readPandocOptionalBiblio
+              <&> writePandoc
+              >>= loadAndApplyTemplate "templates/post.html"    postCtx
+              >>= loadAndApplyTemplate "templates/default.html" postCtx
+              >>= relativizeUrls
+
+    match "assets/csl/*" $ compile cslCompiler
+
+    match "assets/bib/*" $ compile biblioCompiler
+
+    create ["archive.html"] $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let archiveCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Archives"            `mappend`
+                    defaultContext
+
+            makeItem ""
+                >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
+                >>= loadAndApplyTemplate "templates/default.html" archiveCtx
+                >>= relativizeUrls
+
+
+    match "index.html" $ do
+        route idRoute
+        compile $ do
+            posts <- recentFirst =<< loadAll "posts/*"
+            let indexCtx =
+                    listField "posts" postCtx (return posts) `mappend`
+                    constField "title" "Home"                `mappend`
+                    defaultContext
+
+            getResourceBody
+                >>= applyAsTemplate indexCtx
+                >>= loadAndApplyTemplate "templates/default.html" indexCtx
+                >>= relativizeUrls
+
+    match "templates/*" $ compile templateBodyCompiler
+
+
+--------------------------------------------------------------------------------
+postCtx :: Context String
+postCtx =
+    dateField "date" "%B %e, %Y" `mappend`
+    defaultContext
+
+(<&>) :: Functor f => f a -> (a -> b) -> f b
+x <&> f = f <$> x
+
+readPandocOptionalBiblio :: Compiler (Item Pandoc)
+readPandocOptionalBiblio = do
+  item <- getUnderlying
+  maybebib <- getMetadataField item "bibliography"
+  case maybebib of
+    Nothing -> readPandocWith defaultHakyllReaderOptions =<< getResourceBody
+    Just bibFile -> join $
+      readPandocBiblio defaultHakyllReaderOptions
+                   <$> load (fromFilePath "assets/csl/chicago.csl")
+                   <*> load (fromFilePath ("assets/bib/" ++ bibFile))
+                   <*> getResourceBody
